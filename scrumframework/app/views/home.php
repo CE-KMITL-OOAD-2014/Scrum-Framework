1deea9f1875ebe264418f0eff7f9186d3517427f
dscanon <dschanoncanon@gmail.com>
2014-08-22 19:06:50 +0700

    Add folder scrumframework to repository (Initial Project)

---
+++ scrumframework/app/views/home.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+	<link rel="stylesheet" type="text/css" href="media/css/bootstrap.min.css">	
+	<head>
+	<body>
+			
+		<script src="media/js/jquery-1.11.1.min.js"></script>
+		<script src="media/js/jquery-1.11.1.min.map"></script>		
+		<script src="media/js/bootstrap.min.js"></script>				
+	</body>
+</html>
