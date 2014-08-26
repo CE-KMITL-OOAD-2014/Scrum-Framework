9849cf5060163915cff023258844cdcf92acc39b
Choestelus <sprintf.null@gmail.com>
2014-08-26 18:38:59 +0700

    configure login routes

--- scrumframework/app/routes.php
+++ scrumframework/app/routes.php
@@ -18,6 +18,7 @@ Route::get('/', function()
 
 Route::get('login', array('uses' => 'HomeController@showLogin'));
 Route::post('login', array('uses' => 'HomeController@doLogin'));
+Route::get('logout', array('uses' => 'HomeController@doLogout'));
 
 Route::get('/inside', function()
 {
