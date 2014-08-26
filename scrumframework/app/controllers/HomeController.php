6c36516a3e0eb26c15952150fafa4b18b806a2fb
Choestelus <sprintf.null@gmail.com>
2014-08-26 18:39:21 +0700

    configure login controller

--- scrumframework/app/controllers/HomeController.php
+++ scrumframework/app/controllers/HomeController.php
@@ -26,6 +26,43 @@ class HomeController extends BaseController {
 	public function doLogin()
 	{
 		// TODO : process login from
+		$rules = array(
+			'email' => 'required|email',
+			'password' => 'required|alphanum|min:3'
+		);
+		$validator = Validator::make(Input::all(), $rules);
+
+		// if the validator fails, redirect back to the form
+		if ($validator->fails()) {
+			return Redirect::to('login')
+				->withErrors($validator) // send back all errors to the login form
+				->withInput(Input::except('password')); // send back the input (not the password) so that we can repopulate the form
+		} else {
+
+			// create our user data for the authentication
+			$userdata = array(
+				'email' 	=> Input::get('email'),
+				'password' 	=> Input::get('password')
+			);
+
+			// attempt to do the login
+			if (Auth::attempt($userdata)) {
+
+				// validation successful!
+				// redirect them to the secure section or whatever
+				// return Redirect::to('secure');
+				// for now we'll just echo success (even though echoing in a controller is bad)
+				echo 'SUCCESS!';
+
+			} else {	 	
+
+				// validation not successful, send back to form	
+				return Redirect::to('login');
+
+			}
+
+		}
+		
 	}
 
 }
