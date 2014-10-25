6dcbb9bea47ce64e673c4b9406731352bbe58617
Chanon-SVN <chanon@kmi.tl>
2014-10-26 00:00:43 +0700

    add name in input

--- scrumframework/app/views/main.blade.php
+++ scrumframework/app/views/main.blade.php
@@ -15,7 +15,7 @@
 <div class="col-md-6 col-md-offset-6">
 	<form method="post" action="{{url('/addteam')}}">
 		<div class="col-xs-3">
-    		<input type="text" class="form-control" placeholder="Team name">
+    		<input type="text" class="form-control" placeholder="Team name" name="teanname">
   		</div>
 		<button type="submit" class="btn primary">Add Team</button>
 	</form>
