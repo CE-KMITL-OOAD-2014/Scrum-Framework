fedba6c16a30827f58483e4fdec460050726b90f
Chanon-SVN <chanon@kmi.tl>
2014-11-11 18:22:10 +0700

    add Sprintbacklog & Productbacklog Successful

--- scrumframework/app/controllers/SprintnameController.php
+++ scrumframework/app/controllers/SprintnameController.php
@@ -38,15 +38,41 @@ class SprintnameController extends BaseController {
 		// elseif($data['list']=='list3'){
 		// 	$currentboard->list3 = $data['pbacklog'];
 		// }
+		//$currentboard->list1 = array();
+		$currentboard->delete('list0');
+		$currentboard->unset('list0');
+		
+		$currentboard->delete('list1');
+		$currentboard->unset('list1');
+
+		$currentboard->delete('list2');
+		$currentboard->unset('list2');
+
+		$currentboard->delete('list3');
+		$currentboard->unset('list3');
+
+		$currentboard->list0 = array();
+		$currentboard->list1 = array();
+		$currentboard->list2 = array();
+		$currentboard->list3 = array();
+		// $currentboard->unset('list2');
+		// $currentboard->unset('list3');
 		$currentboard->list0 = $data['list0'];
 		$currentboard->list1 = $data['list1'];
 		$currentboard->list2 = $data['list2'];
-		$currentboard->list3 = $data['list1'];
+		$currentboard->list3 = $data['list3'];
 		$currentboard->save();
-		$board->save($currentboard);
+		$board = $board->save($currentboard);
+		 // $currentboard->list0 = $data['list0'];
+		 // $currentboard->list1 = $data['list1'];
+		 // $currentboard->list2 = $data['list2'];
+		 // $currentboard->list3 = $data['list3'];
+		 // $currentboard->save();
+		 //$currentboard = $currentboard->save($currentboard);
+	//	 $board = $board->save($currentboard);
 		fwrite($myfile, $team['name']);
 		fwrite($myfile, "Board = ");
-	//	fwrite($myfile, json_encode($currentboard->list1));
+		fwrite($myfile, json_encode($data['list1']));
 		fwrite($myfile, "List ");
 		fclose($myfile);
 	}
