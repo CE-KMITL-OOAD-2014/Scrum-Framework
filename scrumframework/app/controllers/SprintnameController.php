c7b39bf98a62ccda0af148f437db7300a9e85344
Choestelus <sprintf.null@gmail.com>
2014-11-15 14:21:57 +0700

    remove file debug operations

--- scrumframework/app/controllers/SprintnameController.php
+++ scrumframework/app/controllers/SprintnameController.php
@@ -3,43 +3,37 @@
 class SprintnameController extends BaseController {
 
 
-	public function store()
-	{
-		$data = Input::all();
-		$myfile = fopen("/home/dscanon/newfile.txt", "w") or die("Unable to open file!");
-		$team = Team::find($data['team']);
-		$board = $team->taskboards();
-		$currentboard = $board->find($data['board']);
+    public function store()
+    {
+        $data = Input::all();
+        $team = Team::find($data['team']);
+        $board = $team->taskboards();
+        $currentboard = $board->find($data['board']);
 
-		$currentboard->delete('list0');
-		$currentboard->unset('list0');
-		
-		$currentboard->delete('list1');
-		$currentboard->unset('list1');
+        $currentboard->delete('list0');
+        $currentboard->unset('list0');
 
-		$currentboard->delete('list2');
-		$currentboard->unset('list2');
+        $currentboard->delete('list1');
+        $currentboard->unset('list1');
 
-		$currentboard->delete('list3');
-		$currentboard->unset('list3');
+        $currentboard->delete('list2');
+        $currentboard->unset('list2');
 
-		$currentboard->list0 = array();
-		$currentboard->list1 = array();
-		$currentboard->list2 = array();
-		$currentboard->list3 = array();
+        $currentboard->delete('list3');
+        $currentboard->unset('list3');
 
-		$currentboard->list0 = $data['list0'];
-		$currentboard->list1 = $data['list1'];
-		$currentboard->list2 = $data['list2'];
-		$currentboard->list3 = $data['list3'];
-		$currentboard->save();
-		$board = $board->save($currentboard);
+        $currentboard->list0 = array();
+        $currentboard->list1 = array();
+        $currentboard->list2 = array();
+        $currentboard->list3 = array();
 
-		fwrite($myfile, $team['name']);
-		fwrite($myfile, "Board = ");
-		fwrite($myfile, json_encode($data['list1']));
-		fwrite($myfile, "List ");
-		fclose($myfile);
-	}
+        $currentboard->list0 = $data['list0'];
+        $currentboard->list1 = $data['list1'];
+        $currentboard->list2 = $data['list2'];
+        $currentboard->list3 = $data['list3'];
+        $currentboard->save();
+        $board = $board->save($currentboard);
+
+    }
 
 }
