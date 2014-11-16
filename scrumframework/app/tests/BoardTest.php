fe743f27c17cdb60dcd67a4f819726de1c307666
Chanon-SVN <chanon@kmi.tl>
2014-11-16 08:40:48 +0700

    remove comment in Boardtest

--- scrumframework/app/tests/BoardTest.php
+++ scrumframework/app/tests/BoardTest.php
@@ -9,13 +9,11 @@ class BoardTest extends TestCase {
 	 */
 	public function testBoard()
 	{
-	//	$response = $this->call('GET', '/taskboard/{id}/{bid}');
-		
-		// $taskboardController = new TaskboardController;
-		// $id = "544f7ec36b26f73c048b457d";
-		// $bid = "544fc1786b26f72b1b8b4571";
-  //  		$taskboardController = $taskboardController->processGetTaskboard($id, $bid);
-  //  		$this->assertEquals('ExampleBoard', $taskboardController);
+		$taskboardController = new TaskboardController;
+		$id = "544f7ec36b26f73c048b457d";
+		$bid = "544fc1786b26f72b1b8b4571";
+   		$taskboardController = $taskboardController->processGetTaskboard($id, $bid);
+   		$this->assertEquals('ExampleBoard', $taskboardController);
 	}
 
 }
